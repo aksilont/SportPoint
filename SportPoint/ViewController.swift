9011e33f864a8e1d9d057285d6a573e149fae08a
aksilont <aksilont@gmail.com>
2023-03-06 22:16:48 +0300

    Инициализация проекта XCode

---
+++ SportPoint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportPoint
+//
+//  Created by Aksilont on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
